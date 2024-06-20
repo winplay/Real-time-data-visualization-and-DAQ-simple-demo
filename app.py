ec56694cde996a551da0e87052d2a8b2bb00c4bf
CH Hsueh <xch210@qq.com>
2024-06-21 00:09:45 +0800

    增加左方输入； 增加右方第一栏感兴趣的值； 将右方第二栏该有1x2布局 增加第三栏统计功能

--- app.py
+++ app.py
@@ -4,13 +4,37 @@ import dash_daq as daq
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import dash_bootstrap_components as dbc
 import numpy as np
+from datetime import datetime
 
 
 # Initialize the app - incorporate a Dash Bootstrap theme
 external_stylesheets = [dbc.themes.CERULEAN]
-app = Dash(__name__)#, external_stylesheets=external_stylesheets)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+###----------------生成左边栏----------------###
+def Generate_input_ip_address(id='input_ip_address'):
+    """
+    生成输入 IP地址栏
+    """
+    return html.Div(children=[
+        dbc.Label("Input Ip Address and port:"),
+        dbc.Input(placeholder="Input Ip here...", value='192.168.1.10',type="text",id=id),
+        dbc.FormText("time tagger IP 地址"),
+    ])
+
+def Generate_input_time_intergration(id='input_time_intergration'):
+    """
+    时间积分
+    """
+    return html.Div(children=[
+        dbc.Label("输入时间积分窗口[us]:"),
+        dbc.Input(placeholder="Input us here...", value='10',type="number",id=id),
+        dbc.FormText("在时间窗口内计数"),
+    ])
+
 
 ###----------------生成右边栏----------------###
 #####--------------生成第一栏--------------#####
@@ -24,13 +48,31 @@ def Generate_r_c_row1():
 
 #####--------------生成第二栏--------------#####
 
+def Generate_time_phi_graph():
+    """
+    生成图表
+    """
+    fig_time_phi = make_subplots(rows=1, cols=2)
+
+    fig_time_phi.add_trace(
+        go.Scatter(x=[1, 2, 3], y=[4, 5, 6],mode='lines', name='$\Delta$'),
+        row=1, col=1
+    )
+
+    fig_time_phi.add_trace(
+        go.Scatter(x=[20, 30, 40], y=[50, 60, 70]),
+        row=1, col=2
+    )
+    
+    return fig_time_phi
+
 def Generate_r_c_row2():
     """
     生成右边第二栏
     包含图表
     """
     return dbc.Row(id='r_c_row2',children=[
-        dcc.Graph(figure=px.line(x=[0],y=[0]), id='time_phi_graph'),
+        dcc.Graph(figure=Generate_time_phi_graph(), id='time_phi_graph',mathjax=True),
         dcc.Graph(figure=px.line(x=[0],y=[0]), id='frequency_phi_graph') 
         ])
 
@@ -42,15 +84,7 @@ def Generate_cps_phi_graph():
     fig_cps_phi = go.Figure()
     fig_cps_phi.add_trace(go.Scatter(
         x=[],
-        y=[],
-        xaxis='x',
-        yaxis='y',
-        mode='markers',
-        marker=dict(
-            color='rgba(0,64,128,1)',
-            size=3
-        )))
-    fig_cps_phi.update_layout(margin={'l': 5, 'r': 5, 'b': 5, 't': 5, 'pad': 0})
+        y=[]))
     return fig_cps_phi
 
 
@@ -75,10 +109,8 @@ app.layout = dbc.Container([
 
     dbc.Row(id='main windows',children=[
         dbc.Col(id='left columns',width='auto',children=[
-            dbc.RadioItems(options=[{"label": x, "value": x} for x in ['pop', 'lifeExp', 'gdpPercap']],
-                       value='lifeExp',
-                       inline=True,
-                       id='radio-buttons-final')
+            Generate_input_ip_address(),
+            Generate_input_time_intergration(),
         ]),
 
         dbc.Col(id='right columns',width='auto',children=[
@@ -90,27 +122,39 @@ app.layout = dbc.Container([
 
 ], fluid=True)
 
-cps_std=[]
+data_dict=pd.DataFrame(columns=['date','SR[KHz]','shot noise','cps','contrast','std','allan']).to_dict()
 # Add controls to build the interaction
 @callback(
     [Output(component_id='time_phi_graph', component_property='extendData'),
     Output(component_id='frequency_phi_graph', component_property='extendData'),
-    Output(component_id='cps_phi_graph', component_property='extendData')],
+    Output(component_id='r_c_row1', component_property='children')],
     Input(component_id='interval_component', component_property='n_intervals'),
-    Input(component_id='flag_cps_std', component_property='on')
+    Input(component_id='input_time_intergration', component_property='value')
 )
-def update_graph(n,flag_cps_std):
+def update_graph(n,time_intergration):
+    time_intergration=float(time_intergration)
     data=np.random.random(20)
     data_time=np.arange(data.size)
     freq_data=np.abs(np.fft.rfft(data))
     freq=np.fft.rfftfreq(data.size,d=1/data.size)
     time_fig = [dict( x=[data_time],y=[data]), [0], data.size]
-    freq_fig=[dict( x=[freq],y=[freq_data]), [0], freq.size]
+    freq_fig = [dict( x=[freq],y=[freq_data]), [0], freq.size]
+    
+    data_dict=pd.DataFrame(np.random.rand(1,7),columns=['date','SR[KHz]','shot noise','cps','contrast','std','allan'])
+    
+    return [time_fig, freq_fig,dbc.Table.from_dataframe(pd.DataFrame(data_dict),id='out_put_table')]
+
+@callback(
+    [Output(component_id='cps_phi_graph', component_property='extendData')],
+    Input(component_id='interval_component', component_property='n_intervals'),
+    Input(component_id='flag_cps_std', component_property='on')
+)
+def update_graph(n,flag_cps_std):
     if flag_cps_std:
-        cps_fig=[dict( y=[[np.std(data)]]), [0], 10]
+        cps_fig=[dict( x=[[n]],y=[[1+n]]), [0], 100]
     else:
-        cps_fig=[dict(x=[[0]], y=[[0]]), [0], 10]
-    return [time_fig, freq_fig, cps_fig]
+        cps_fig=[dict(x=[[]], y=[[]]), [0], 100]
+    return [cps_fig]
 
 # Run the app
 if __name__ == '__main__':
